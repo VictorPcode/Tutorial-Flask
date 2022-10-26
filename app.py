7c76f0af7896ef1c5d1dd8d0480f626d4bf07229
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2022-10-26 15:28:23 -0300

    app factory y blueprints practicados y funcionando

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ todos = ['Comprar cafe', 'Solicitud de compra ', 'Enviar video al productor']
 
 @app.cli.command()
 def test():
-    test = unittest.TestLoader().discover('test')
-    unittest.TextTestRunner().run(test)
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.route('/')
 def index():
@@ -21,25 +22,14 @@ def index():
     
     return response
 
-@app.route('/inicio', methods=['GET', 'POST'])
+@app.route('/inicio', methods=['GET'])
 def hola():
     user_ip= session.get('user_ip')
-    Login_Form = LoginForm()
     username = session.get('username')
-    
     context= {'user_ip': user_ip,
           'todos': todos,
-          'login_form': Login_Form,
           'username': username,
         }
-    
-    if Login_Form.validate_on_submit():
-        username = Login_Form.username.data
-        session['username'] = username
-        flash('Nombre de usuario registrado con exito!')
-        
-        return redirect(url_for('index'))
-    
     return render_template('hola.html', **context)
 
 @app.errorhandler(404)
