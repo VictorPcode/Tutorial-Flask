819deab959a2fcd51f1b1a0709ef8ffff6de1807
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2022-10-26 11:19:58 -0300

    en proceso de ejecucion de un factory app en el proyecto todo list

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
-from flask_bootstrap import Bootstrap
-from flask import Flask, jsonify, make_response, request, redirect, render_template
+from flask import flash, jsonify, make_response, session, request, redirect, render_template, url_for
+import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app= Flask(__name__,template_folder="template", static_folder="static")
-bootstrap = Bootstrap(app)
+app= create_app()
 
 todos = ['Comprar cafe', 'Solicitud de compra ', 'Enviar video al productor']
+
+
+@app.cli.command()
+def test():
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner().run(test)
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect('/hola'))
-    response.set_cookie('user_ip', user_ip)
+    response = make_response(redirect('/inicio'))
+    session['user_ip'] = user_ip
     
     return response
-    
-@app.route('/hola')
+
+@app.route('/inicio', methods=['GET', 'POST'])
 def hola():
-    user_ip= request.cookies.get('user_ip')
+    user_ip= session.get('user_ip')
+    Login_Form = LoginForm()
+    username = session.get('username')
+    
     context= {'user_ip': user_ip,
-          'todos': todos,}
-    return render_template('/hola.html', **context)
+          'todos': todos,
+          'login_form': Login_Form,
+          'username': username,
+        }
+    
+    if Login_Form.validate_on_submit():
+        username = Login_Form.username.data
+        session['username'] = username
+        flash('Nombre de usuario registrado con exito!')
+        
+        return redirect(url_for('index'))
+    
+    return render_template('static/template/hola.html', **context)
 
 @app.errorhandler(404)
 def error_notFound(error):
